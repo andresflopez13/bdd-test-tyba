ed6393f07fa587043230a293e45faa1401f041a7
Andres Fernando Lopez Avila <andres.lopez@rappi.com>
2021-06-12 22:47:19 -0500

    Task
    - Se crea la task para ir hasta la calculadora donde se realizaran la prueba definida

---
+++ src/main/java/co/com/tyba/test/metrocuadrado/tasks/GoTo.java
@@ -0,0 +1,12 @@
+package co.com.tyba.test.metrocuadrado.tasks;
+
+import static net.serenitybdd.screenplay.Tasks.instrumented;
+
+public class GoTo {
+
+  private GoTo() {}
+
+  public static HowMuchDoTheyLendYou howMuchDoTheyLendYouCalculator() {
+    return instrumented(HowMuchDoTheyLendYou.class);
+  }
+}
---
+++ src/main/java/co/com/tyba/test/metrocuadrado/tasks/HowMuchDoTheyLendYou.java
@@ -0,0 +1,20 @@
+package co.com.tyba.test.metrocuadrado.tasks;
+
+import static co.com.tyba.test.metrocuadrado.userinterface.Home.BTN_HOUSING_MORTGAGE_CALCULATOR;
+import static co.com.tyba.test.metrocuadrado.userinterface.Home.BTN_SELECT_TOOLS;
+
+import co.com.tyba.test.metrocuadrado.interactions.TabBrowser;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.actions.Click;
+import net.thucydides.core.annotations.Step;
+
+public class HowMuchDoTheyLendYou implements Task {
+
+  @Override
+  @Step("{0} go to the pivots section")
+  public <T extends Actor> void performAs(T actor) {
+    actor.attemptsTo(
+        Click.on(BTN_SELECT_TOOLS), Click.on(BTN_HOUSING_MORTGAGE_CALCULATOR), TabBrowser.change());
+  }
+}
